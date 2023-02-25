7556d2d41569352afed56118cb1368945a91197a
introbond <introbond.dev@gmail.com>
2023-02-25 12:57:20 +0700

    update: contact css

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -13,36 +13,38 @@ const Contact = () => {
    
     return (
 
-        <div className="contact">
-            
-            <div className="contact-info">
-                <p> <strong> Name: </strong> James-Bond, Puritat Chamart</p>
-                <p> <strong> Social Media: </strong> </p>
-                <p> <strong> - GitHub: </strong> https://github.com/introbond </p>
-                <p> <strong> - Replit: </strong> https://replit.com/@introbond </p>
-                <p> <strong> - Email: </strong> introbond.dev@gmail.com </p>
-            </div>
+        <div className="background">
+                <div className="contact">
+                    <h1> C O N T A C T </h1>
+                    <div className="contact-info">
+                        <p> <strong> Name: </strong> James-Bond, Puritat Chamart</p>
+                        <p> <strong> Social Media: </strong> </p>
+                        <p> <strong> - GitHub: </strong> https://github.com/introbond </p>
+                        <p> <strong> - Replit: </strong> https://replit.com/@introbond </p>
+                        <p> <strong> - Email: </strong> introbond.dev@gmail.com </p>
+                    </div>
 
-            <div className="github">
-                <Github/>
-            </div>
+                    <div className="github">
+                        <Github/>
+                    </div>
 
-            <div className="typewriter">
-                <Typewriter
-                    onInit={(typewriter) => {
-                        typewriter.start()
-                            .typeString('Still finding myself.')
-                            .pauseFor(10)
-                            .typeString('<strong>     - James Bond </strong>')
-                    }}
-                />
-            </div>
+                    <div className="typewriter">
+                        <Typewriter
+                            onInit={(typewriter) => {
+                                typewriter.start()
+                                    .typeString('Still finding myself.')
+                                    .pauseFor(10)
+                                    .typeString('<strong>     - James Bond </strong>')
+                            }}
+                        />
+                    </div>
 
-            <div className="socialMedia">
-                    <IconGithub onClick={() => openInNewTab('https://github.com/introbond')} />
-                    <SiReplit onClick={() => openInNewTab('https://replit.com/@introbond')}/>
-            </div> 
+                    <div className="socialMedia">
+                            <IconGithub onClick={() => openInNewTab('https://github.com/introbond')} />
+                            <SiReplit onClick={() => openInNewTab('https://replit.com/@introbond')}/>
+                    </div> 
 
+                </div>
         </div>
     );
 };
